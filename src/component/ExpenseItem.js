0846be89efe30f5d2f0f0635e2f0218e3d2301ba
pnk-85 <pnkghogare27@gmail.com>
2023-07-13 16:08:55 +0530

    Create Expense And Item

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,25 @@
+function ExpenseItem () {
+    return (
+        <div>
+            <div>11/07/2023</div>
+            <div>
+            <h2>FOOD</h2>
+            <div>RS 10</div>
+            </div>
+            <br></br>
+            <div>11/07/2023</div>
+            <div>
+            <h2>PETROL</h2>
+            <div>RS 100</div>
+            <br></br>
+            </div><div>11/07/2023</div>
+            <div>
+            <h2>MOVIE</h2>
+            <div>RS 200</div>
+            </div>
+        </div>
+    
+    )
+}
+
+export default ExpenseItem; 
