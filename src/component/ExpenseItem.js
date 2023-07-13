f7ffb1aff54f0e26030939ed48a84ac86f64852a
pnk-85 <pnkghogare27@gmail.com>
2023-07-13 20:12:41 +0530

    using css and variable

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,25 +1,41 @@
+import './ExpenseItem.css'
+
 function ExpenseItem () {
+    const LocationOfExpenditure = 'Pune';
+    const expenseTitle ={name:'FOOd',name1:'PETROL',name2:'MOVIE'};
+    const expensePrice ={price:'RS10',price1:'RS100', price2:'RS200'};
     return (
         <div>
-            <div>11/07/2023</div>
-            <div>
-            <h2>FOOD</h2>
-            <div>RS 10</div>
+            <div className='expense-item'>
+            <div>13/07/2023 </div>
+            <div className='expense-item__description'>
+            <h2>{expenseTitle.name}</h2>
+            <div className='expense-item__location'>{LocationOfExpenditure}</div>
+            <div className='expense-item__price'>{expensePrice.price}</div>
+            </div>
             </div>
             <br></br>
-            <div>11/07/2023</div>
-            <div>
-            <h2>PETROL</h2>
-            <div>RS 100</div>
+            <div className='expense-item'>
+            <div>13/07/2023 </div>
+            <div className='expense-item__description'>
+            <h2>{expenseTitle.name1}</h2>
+            <div className='expense-item__location'>{LocationOfExpenditure}</div>
+            <div className='expense-item__price'>{expensePrice.price1}</div>
+            </div>
+            </div>
             <br></br>
-            </div><div>11/07/2023</div>
-            <div>
-            <h2>MOVIE</h2>
-            <div>RS 200</div>
+            <div className='expense-item'>
+            <div>13/07/2023 </div>
+            <div className='expense-item__description'>
+            <h2>{expenseTitle.name2}</h2>
+            <div className='expense-item__location'>{LocationOfExpenditure}</div>
+            <div className='expense-item__price'>{expensePrice.price2}</div>
+            </div>
             </div>
         </div>
+        
     
-    )
+    );
 }
 
 export default ExpenseItem; 
