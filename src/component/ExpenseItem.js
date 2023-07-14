1d919923768af5b1e7621400509ab09efe088e9e
pnk-85 <pnkghogare27@gmail.com>
2023-07-14 12:09:55 +0530

    props used

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,38 +1,16 @@
 import './ExpenseItem.css'
 
-function ExpenseItem () {
-    const LocationOfExpenditure = 'Pune';
-    const expenseTitle ={name:'FOOd',name1:'PETROL',name2:'MOVIE'};
-    const expensePrice ={price:'RS10',price1:'RS100', price2:'RS200'};
+function ExpenseItem (props) {
+
     return (
-        <div>
-            <div className='expense-item'>
-            <div>13/07/2023 </div>
+        <div className='expense-item'>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-            <h2>{expenseTitle.name}</h2>
-            <div className='expense-item__location'>{LocationOfExpenditure}</div>
-            <div className='expense-item__price'>{expensePrice.price}</div>
-            </div>
-            </div>
-            <br></br>
-            <div className='expense-item'>
-            <div>13/07/2023 </div>
-            <div className='expense-item__description'>
-            <h2>{expenseTitle.name1}</h2>
-            <div className='expense-item__location'>{LocationOfExpenditure}</div>
-            <div className='expense-item__price'>{expensePrice.price1}</div>
-            </div>
-            </div>
-            <br></br>
-            <div className='expense-item'>
-            <div>13/07/2023 </div>
-            <div className='expense-item__description'>
-            <h2>{expenseTitle.name2}</h2>
-            <div className='expense-item__location'>{LocationOfExpenditure}</div>
-            <div className='expense-item__price'>{expensePrice.price2}</div>
-            </div>
+            <h2>{props.title}</h2>
+            <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
+        
         
     
     );
