ba4ab28a22300ea2638757f10288641a42a97758
pnk-85 <pnkghogare27@gmail.com>
2023-07-15 10:00:11 +0530

    using React.createElement and organised files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import ExpenseItem from "./component/ExpenseItem";
+import React from "react";
+import Expenses from "./component/Expenses/Expenses";
+import './App.css';
+
 
 function App() {
 
@@ -32,16 +35,28 @@ function App() {
       location:'chennai'
     },
   ]
-  return (
-    <div>
-      <h2>Lets get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItem>
-      
-    </div>
+
+  return React.createElement (
+    'div',{},React.createElement('h2',{},"Lets get started"),
+    React.createElement(Expenses, {items: expenses})
   );
+  // return (
+  //   <div>
+  //     <h2>Lets get started</h2>
+  //     <Expenses items={expenses}/>
+  //   </div>
+  // );
+  // return (
+
+  //   <div className='expense-item'>
+  //   <ExpenseDate date ={props.date} />
+  //   <div>
+  //   <h1>Expense 1</h1>
+  //   <h1>Expense 1</h1>
+  //   </div>
+  //   <div>
+    
+  //   )
 }
 
 export default App;
--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -1,6 +1,9 @@
+import React from "react";
 import ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
+
 
 
 
@@ -9,10 +12,10 @@ function ExpenseItem (props) {
 
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date ={props.date} />
             <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-        </div>
+        </Card>
         
         
     
