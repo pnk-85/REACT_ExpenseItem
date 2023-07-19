97e53ad8187fb9c31d0f62c84dab6ed1b0efbda0
pnk-85 <pnkghogare27@gmail.com>
2023-07-19 10:18:14 +0530

    Add Delete Button

--- src/component/Expenses/ExpenseItem.js
+++ src/component/Expenses/ExpenseItem.js
@@ -9,12 +9,15 @@ import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
-
+const submit = () => {
+    console.log('Delete Expense');
+}
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date ={props.date} />
             <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+            <button onClick={submit}>Delete Expense</button>
         </Card>
         
         
