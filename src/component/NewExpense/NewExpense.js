7c4898693fee763f3d3d173840677130c357b5ec
pnk-85 <pnkghogare27@gmail.com>
2023-07-21 18:54:11 +0530

    adding cancel button and add new expense

--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
 const NewExpense = (props) => {
+   const [isEditing, setIsEditing] = useState(false);
 
     const expenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -9,10 +11,20 @@ const NewExpense = (props) => {
             id:Math.random().toString()
         };
         props.onaddExpense(expenseData);
+        setIsEditing(false);
     };
+
+    const startEditingHandler = () => {
+        setIsEditing(true);    
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false);
+    }
     return (
         <div className='new-expense'>
-            <ExpenseForm onExpenseData={expenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+            {isEditing && <ExpenseForm onExpenseData={expenseDataHandler} onCancel={stopEditingHandler}/>}
 
         </div>
     )
